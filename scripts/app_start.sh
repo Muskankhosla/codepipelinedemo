dee1b39f7e777f24bbef9673e409fb40e42d0e8f
Muskan Khosla <khosla.muskan01@gmail.com>
2024-02-01 14:31:32 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -3,11 +3,14 @@
 # Change Working Directory
 cd /home/ec2-user/server
 
+# Identify the full path to pm2
+PM2_PATH=$(which pm2)
+
 # Install PM2 if not already installed
 npm install -g pm2
 
 # Delete Old PM2 Service, if exists
-pm2 delete Frontend
+$PM2_PATH delete Frontend
 
 # Start the PM2 service
-pm2 start server.js --name Frontend
+$PM2_PATH start server.js --name Frontend
