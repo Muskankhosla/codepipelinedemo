4f21b881dce1a57f0a2d8cd7f7d3121ba8a7e0cd
Muskan Khosla <khosla.muskan01@gmail.com>
2024-02-01 13:09:46 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #_Change_Working_Directory
-cd /home/ubuntu
+cd /home/ec2-user/server
 
 #_Update_&_Set_Node_Version
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 
 #_Download_Node_&NPM
-sudo apt-get install -y nodejs
+yum -y install nodejs npm
 
 #_Download_PM2
-sudo npm install pm2@latest -g
+npm install pm2@latest -g
