686eeb687b6247d3d334510edc7a47887c2ea508
Muskan Khosla <khosla.muskan01@gmail.com>
2024-02-01 14:15:52 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-#_Change_Working_Directory
+# Change Working Directory
 cd /home/ec2-user/server
 
-#_Remove_Unused_Code
+# Remove Unused Code
 rm -rf node_modules
 rm -rf build
 
-#Install_node_modules_&_Make_React_Build
-npm  install
+# Update npm and Babel dependencies
+npm install -g npm
+npm install --save-dev @babel/plugin-transform-class-properties \
+  @babel/plugin-transform-numeric-separator \
+  @babel/plugin-transform-nullish-coalescing-operator \
+  @babel/plugin-transform-optional-chaining \
+  @babel/plugin-transform-private-methods
+
+# Install Node Modules and Build
+npm install
 npm run build
