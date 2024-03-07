41d4057d3553fde159559edd018a71af08a47c32
Muskan Khosla <khosla.muskan01@gmail.com>
2024-03-07 12:32:35 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -2,7 +2,8 @@
 
 # Change Working Directory
 cd /home/ec2-user/server
-
+# PM2 is process manager for Node.js applications that allows you to keep your applications running continuously
+# PM2 can automatically restart your application if it crashes, & reload your application when you update your code.
 # Install PM2 if not already installed
 npm install -g pm2
 
